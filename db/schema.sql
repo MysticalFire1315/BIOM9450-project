591d93960d1a9a8220b1ad46008bc3b788242adc
MysticalFire1315 <joelchong03@gmail.com>
2024-10-04 11:29:05 +1000

    Use only users table

--- db/schema.sql
+++ db/schema.sql
@@ -4,49 +4,41 @@
 -- Cancer Types Table: List of cancer types like breast cancer, lung cancer, etc.
 -- Patient Table: Contains patient data including ID, name, photo, diagnosis, and associated mutations.
 -- Researcher/Oncologist Table: Info about the professionals using the system, including their specialties.
-DROP DATABASE IF EXISTS "BIOM9450_project";
-
-CREATE DATABASE IF NOT EXISTS "BIOM9450_project";
-
-USE "BIOM9450_project";
-
 ----------
 -- Table structure for table "users"
 ----------
+DROP TABLE IF EXISTS "users";
 
 CREATE TABLE "users" (
-  "id" INTEGER NOT NULL AUTO_INCREMENT,
+  "id" SERIAL PRIMARY KEY,
   "username" VARCHAR(255),
   "password_hash" VARCHAR(255),
   "salt" VARCHAR(255),
-  "created_at" TIMESTAMP,
-  "role" ENUM('admin', 'researcher', 'oncologist'),
-  PRIMARY KEY ("id")
+  "created_at" TIMESTAMP DEFAULT current_timestamp
 );
 
-----------
--- Table structure for table "People"
-----------
-DROP TABLE IF EXISTS "People";
+-- ----------
+-- -- Table structure for table "People"
+-- ----------
+-- DROP TABLE IF EXISTS "People";
 
-CREATE TABLE "People" (
-  "id" INTEGER NOT NULL AUTO_INCREMENT,
-  "name" VARCHAR(255) NOT NULL,
-  "age" INTEGER,
-  PRIMARY KEY ("id")
-);
+-- CREATE TABLE "People" (
+--   "id" SERIAL,
+--   "name" VARCHAR(255) NOT NULL,
+--   "age" INTEGER,
+--   PRIMARY KEY ("id")
+-- );
 
-----------
--- Table structure for table "Patients"
-----------
-DROP TABLE IF EXISTS "Patients";
+-- ----------
+-- -- Table structure for table "Patients"
+-- ----------
+-- DROP TABLE IF EXISTS "Patients";
 
-CREATE TABLE "Patients" (
-  "id" INTEGER NOT NULL AUTO_INCREMENT,
-  "photo" LONGBLOB,
-  "diagnosis" VARCHAR(255),
-  "mutation_id" INTEGER,
-  PRIMARY KEY ("id"),
-  FOREIGN KEY ("id") REFERENCES "People"("id"),
-  FOREIGN KEY ("mutation_id") REFERENCES "Mutation_Variants"("id")
-);
+-- CREATE TABLE "Patients" (
+--   "id" SERIAL,
+--   "photo" BYTEA,
+--   "diagnosis" VARCHAR(255),
+--   "mutation_id" INTEGER,
+--   PRIMARY KEY ("id"),
+--   FOREIGN KEY ("id") REFERENCES "People"("id") -- FOREIGN KEY ("mutation_id") REFERENCES "Mutation_Variants"("id")
+-- );
