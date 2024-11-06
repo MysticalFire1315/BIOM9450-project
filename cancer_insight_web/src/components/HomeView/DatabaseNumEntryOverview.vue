139a42b083e9aeb54b9fb407422a5caa518935ed
Evan Qiu <evan.qiu@student.unsw.edu.au>
2024-11-06 18:50:43 +1100

    The donut chart to be wrapped inside a card component The form component to be created

---
+++ cancer_insight_web/src/components/HomeView/DatabaseNumEntryOverview.vue
@@ -0,0 +1,31 @@
+<template>
+    <Doughnut :data="data" :options="options" />
+  </template>
+  
+  <script>
+  import { ref } from 'vue'
+  import { Doughnut } from 'vue-chartjs'
+  import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+  // Updated import path
+  import * as chartConfig from '@/config/DatabaseNumEntryOverviewChartConfig.js'
+  
+  ChartJS.register(ArcElement, Tooltip, Legend)
+  
+  export default {
+    name: 'DatabaseNumEntryOverview',
+    components: {
+      Doughnut
+    },
+    setup() {
+      // Define data and options using Composition API
+      const data = ref(chartConfig.data)
+      const options = ref(chartConfig.options)
+  
+      return {
+        data,
+        options
+      }
+    }
+  }
+  </script>
+  
--- cancer_insight_web/src/views/DashboardView.vue
+++ cancer_insight_web/src/views/DashboardView.vue
@@ -1,8 +1,17 @@
 <template>
-  <v-container style="margin-left: 10%;">
+  <v-container style="margin-left: 5%;">
     <v-row style="width:135%">
       <v-col cols="7">
         <GreetingPanel/>
+      </v-col>
+      <v-col cols="5">
+        <IdentityPanel/>
+      </v-col>
+    </v-row>
+
+    <v-row style="width:135%">
+      <v-col cols="7">
+        <DatabaseNumEntryOverview/>
       </v-col>
       <v-col cols="5">
         <IdentityPanel/>
@@ -16,4 +25,5 @@
 <script setup>
 import GreetingPanel from '@/components/HomeView/GreetingPanel.vue'; 
 import IdentityPanel from '@/components/HomeView/IdentityPanel.vue';
+import DatabaseNumEntryOverview from '@/components/HomeView/DatabaseNumEntryOverview.vue';
 </script>
