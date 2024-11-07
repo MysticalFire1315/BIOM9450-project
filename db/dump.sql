424059d7efbf0f85670321ac0f020cd0654c3c61
MysticalFire1315 <joelchong03@gmail.com>
2024-11-07 18:05:41 +1100

    Add sample data for oncologists and researchers

--- db/dump.sql
+++ db/dump.sql
@@ -1,33 +1,15 @@
-INSERT INTO
-  people (
-    firstname,
-    lastname,
-    date_of_birth,
-    sex
-  )
-VALUES
-  ('John', 'Doe', '1985-04-15', 'male'),
-  (
-    'Jane',
-    'Smith',
-    '1978-11-22',
-    'female'
-  ),
-  (
-    'Alex',
-    'Johnson',
-    '1990-03-10',
-    'other'
-  ),
-  (
-    'Maria',
-    'Gonzalez',
-    '1989-07-30',
-    'female'
-  ),
-  (
-    'Liam',
-    'Brown',
-    '1995-12-05',
-    'male'
-  );
+INSERT INTO people (firstname, lastname, date_of_birth, sex)
+    VALUES ('John', 'Doe', '1985-04-15', 'male'),
+    ('Jane', 'Smith', '1978-11-22', 'female'),
+    ('Alex', 'Johnson', '1990-03-10', 'other'),
+    ('Maria', 'Gonzalez', '1989-07-30', 'female'),
+    ('Liam', 'Brown', '1995-12-05', 'male');
+
+INSERT INTO oncologists (people_id)
+    VALUES (1),
+    (3);
+
+INSERT INTO researchers (people_id)
+    VALUES (2),
+    (5);
+
