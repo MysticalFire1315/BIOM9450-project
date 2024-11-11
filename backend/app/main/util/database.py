5f7ba3999b9cd430ebf376aa156d8354fc9afa7f
MysticalFire1315 <joelchong03@gmail.com>
2024-11-11 17:10:41 +1100

    Fixed logging for queries

--- backend/app/main/util/database.py
+++ backend/app/main/util/database.py
@@ -1,23 +1,16 @@
 from contextlib import contextmanager
 from psycopg2 import errors, errorcodes, pool
-
-import os
-import time
 from psycopg2.extras import LoggingConnection, LoggingCursor
-import logging
 
-from flask import current_app
-import traceback
+import time
+import logging
+import textwrap
 
 # Logging psycopg2 -> https://stackoverflow.com/a/14095624
 class LoggingConnection2(LoggingConnection):
     def filter(self, msg, curs):
         t = (time.time() - curs.timestamp) * 1000
-        try:
-            return msg.decode() + os.linesep + f'Query executed in: {t:.2f} ms. {curs.rowcount} row(s) affected.'
-        except Exception:
-            # current_app.logger.error(traceback.format_exc())
-            pass
+        return f'Query executed in: {t:.2f} ms. {curs.rowcount} row(s) affected.'
 
     def cursor(self, *args, **kwargs):
         kwargs.setdefault('cursor_factory', LoggingCursor2)
@@ -25,8 +18,18 @@ class LoggingConnection2(LoggingConnection):
 
 class LoggingCursor2(LoggingCursor):
     def execute(self, query, vars=None):
-        self.timestamp = time.time()
-        return LoggingCursor.execute(self, query, vars)
+        logger = logging.getLogger('psycopg2')
+        query = textwrap.dedent(query)
+        query = f"\n{query}" if query[0] != '\n' else query
+        query = f"{query}\n" if query[-1] != '\n' else query
+        logger.info(query)
+
+        try:
+            self.timestamp = time.time()
+            return LoggingCursor.execute(self, textwrap.dedent(query), vars)
+        except Exception as exc:
+            logging.error(f"{exc.__class__.__name__}: {exc}")
+            raise exc
 
     def callproc(self, procname, vars=None):
         self.timestamp = time.time()
@@ -50,7 +53,7 @@ NotNullViolation = errors.lookup(errorcodes.NOT_NULL_VIOLATION)
 def db_get_cursor():
     try:
         con = postgreSQL_pool.getconn()
-        con.initialize(current_app.logger)
+        con.initialize(logging.getLogger('psycopg2'))
         cur = con.cursor()
         yield cur
         con.commit()
