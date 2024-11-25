aae919836b9613a3cb5be6d4e8ced90f6b98d525
MysticalFire1315 <joelchong03@gmail.com>
2024-11-26 00:37:18 +1100

    Added documentation

--- backend/app/main/service/person_service.py
+++ backend/app/main/service/person_service.py
@@ -7,10 +7,25 @@ from app.main.model.researcher import Researcher
 
 
 def create_person() -> Tuple[Dict[str, str], int]:
+    """Creates a new person entry in the database.
+
+    Returns:
+        Tuple[Dict[str, str], int]: A tuple containing a response message and a status code.
+    """
+
     return {"status": "fail", "message": "not implemented"}, 501
 
 
-def get_person_role(person: Person):
+def get_person_role(person: Person) -> str:
+    """Retrieves the role of a person.
+
+    Args:
+        person (Person): The Person object to retrieve the role for.
+
+    Returns:
+        str: The role of the person. Possible values are "patient", "oncologist", "researcher", or "none".
+    """
+
     role_classes = {
         "patient": Patient,
         "oncologist": Oncologist,
