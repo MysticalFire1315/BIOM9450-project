288f6b20ae8ebc30d434e6ad84ec87181584d805
MysticalFire1315 <joelchong03@gmail.com>
2024-11-26 00:48:54 +1100

    Added documentation

--- backend/app/main/service/user_service.py
+++ backend/app/main/service/user_service.py
@@ -8,10 +8,29 @@ from app.main.util.exceptions.errors import NotFoundError
 
 
 def check_link(user: User) -> Tuple[Dict[str, str], int]:
+    """Checks if the user is linked to a person.
+
+    Args:
+        user (User): The User object to check the link for.
+
+    Returns:
+        Tuple[Dict[str, str], int]: A tuple containing the status and a message.
+    """
+
     return {"status": "success", "message": user.people_id is not None}, 200
 
 
 def link_user(user: User, data: Dict[str, str]) -> Tuple[Dict[str, str], int]:
+    """Links a user to a person.
+
+    Args:
+        user (User): The User object to link.
+        data (Dict[str, str]): A dictionary containing the person ID to link to the user.
+
+    Returns:
+        Tuple[Dict[str, str], int]: A tuple containing the status, a message, and the role of the linked person.
+    """
+
     # Check person_id exists
     person = Person.get_by_id(data.get("person_id"))
     user.people_id = person.id
@@ -23,6 +42,15 @@ def link_user(user: User, data: Dict[str, str]) -> Tuple[Dict[str, str], int]:
 
 
 def get_user(user: User) -> User:
+    """Retrieves the user with their role.
+
+    Args:
+        user (User): The User object to retrieve the role for.
+
+    Returns:
+        User: The User object with the associated role.
+    """
+
     try:
         user.role = get_person_role(Person.get_by_id(user.people_id))
     except NotFoundError:
@@ -31,6 +59,16 @@ def get_user(user: User) -> User:
 
 
 def user_history(user: User, num: int) -> List[Dict[str, str]]:
+    """Retrieves the user's request history.
+
+    Args:
+        user (User): The User object to retrieve the history for.
+        num (int): The number of recent requests to retrieve.
+
+    Returns:
+        List[Dict[str, str]]: A list of dictionaries containing request log details.
+    """
+
     try:
         with db_get_cursor() as cur:
             cur.execute(
@@ -38,7 +76,7 @@ def user_history(user: User, num: int) -> List[Dict[str, str]]:
                 SELECT time_accessed, method, url_path, status_code FROM request_logs
                 WHERE user_id = %s
                 ORDER BY time_accessed DESC;
-            """,
+                """,
                 (user.id,),
             )
             result = cur.fetchall()[:num]
