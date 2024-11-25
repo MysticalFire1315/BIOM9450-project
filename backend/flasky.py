1a118dcdd1d39a45fb6fc4447aca3ae70730a272
MysticalFire1315 <joelchong03@gmail.com>
2024-11-25 22:44:09 +1100

    Remove test mode from app

--- backend/flasky.py
+++ backend/flasky.py
@@ -1,7 +1,4 @@
 import os
-import unittest
-
-import click
 
 from app import blueprint
 from app.main import create_app
@@ -10,17 +7,6 @@ app = create_app(os.getenv("BOILERPLATE_ENV") or "dev")
 app.register_blueprint(blueprint)
 
 app.app_context().push()
-
-
-@app.cli.command()
-@click.option("--coverage/--no-coverage", default=False, help="aaa")
-def test(coverage=False):
-    """Runs the unit tests."""
-    tests = unittest.TestLoader().discover("app/test", pattern="test*.py")
-    result = unittest.TextTestRunner(verbosity=2).run(tests)
-    if result.wasSuccessful():
-        return 0
-    return 1
 
 
 @app.cli.command()
