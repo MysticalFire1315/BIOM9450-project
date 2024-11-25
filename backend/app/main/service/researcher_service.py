5046fc695d6abad398d919dae846b69aa914f25a
MysticalFire1315 <joelchong03@gmail.com>
2024-11-26 00:38:56 +1100

    Added documentation

--- backend/app/main/service/researcher_service.py
+++ backend/app/main/service/researcher_service.py
@@ -2,11 +2,26 @@ from app.main.model.person import Person
 from app.main.model.researcher import Researcher
 
 
-def get_profile(id: int):
-    x = Researcher.get_by_people_id(id)
-    x.person = Person.get_by_id(id)
+def get_profile(people_id: int) -> Researcher:
+    """Retrieves the profile of a researcher by their people ID.
+
+    Args:
+        people_id (int): The people ID of the researcher to retrieve.
+
+    Returns:
+        Researcher: The Researcher object with the associated person details.
+    """
+
+    x = Researcher.get_by_people_id(people_id)
+    x.person = Person.get_by_id(people_id)
     return x
 
 
 def get_all_researchers():
+    """Retrieves all researchers and their associated person details.
+
+    Returns:
+        List[Person]: A list of Person objects representing all researchers.
+    """
+
     return [Person.get_by_id(x.people_id) for x in Researcher.get_all()]
